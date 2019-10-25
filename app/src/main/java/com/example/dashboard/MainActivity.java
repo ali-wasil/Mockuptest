468df36cb6df56fd4e5c02030c4770ab80f44e56
Ali Wasil <wasilgr21@gmail.com>
2019-10-25 19:31:36 +0700

    Update kerjaan

--- app/src/main/java/com/example/dashboard/MainActivity.java
+++ app/src/main/java/com/example/dashboard/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button masuk = findViewById(R.id.masuk);
+        masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent masuk = new Intent(getBaseContext(),login.class);
+                startActivity(masuk);
+            }
+        });
+
+        Button daftar = findViewById(R.id.daftar);
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent daftar = new Intent(getBaseContext(), board.class);
+                startActivity(daftar);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/dashboard/login.java
@@ -0,0 +1,35 @@
+package com.example.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button reg = findViewById(R.id.regris);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent regs = new Intent(getBaseContext(),MainActivity.class);
+                startActivity(regs);
+            }
+        });
+
+        Button log = findViewById(R.id.login);
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent log = new Intent(getBaseContext(), board.class);
+                startActivity(log);
+            }
+        });
+    }
+}
