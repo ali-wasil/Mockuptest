fe7c8232c577f0f3824e2bfd8ac059adeb3d046e
Ali Wasil <wasilgr21@gmail.com>
2019-10-30 00:46:07 +0700

    font pada dashboard lebih centre

--- app/src/main/java/com/example/dashboard/login.java
+++ app/src/main/java/com/example/dashboard/login.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
@@ -29,6 +30,15 @@ public class login extends AppCompatActivity {
             public void onClick(View view) {
                 Intent log = new Intent(getBaseContext(), board.class);
                 startActivity(log);
+            }
+        });
+
+        final TextView forgetps = findViewById(R.id.forgotpsw);
+        forgetps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent forgetps = new Intent(getBaseContext(),forgetpsw.class);
+                startActivity(forgetps);
             }
         });
     }
